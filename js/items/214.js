b060153a2037e5811646207a9315a643e73249fe
xudojnik <AKryukov92@users.noreply.github.com>
2015-08-02 13:59:11 +0600

    ????????? ??????????? ?? ?????????? ????????. ??????????? ???????????? ?????? ??? ??????? ????????????? ?????????.

--- js/items/214.js
+++ js/items/214.js
@@ -2,7 +2,7 @@
 	"name":"Средняя броня вангардов / Биологическая угроза",
 	"category":"armor",
 	"id":214,
-	"protection":118,
+	"protection":96,
 	"lvlreq":11,
 	"description":"Поглощает 35% огненного урона."
 }
--- js/items/215.js
+++ js/items/215.js
@@ -2,7 +2,7 @@
 	"name":"Средняя броня вангардов / Химическая угроза",
 	"category":"armor",
 	"id":215,
-	"protection":118,
+	"protection":96,
 	"lvlreq":11,
 	"description":"Поглощает 50% химического урона."
 }
