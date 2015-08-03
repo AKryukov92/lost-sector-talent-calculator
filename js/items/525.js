31ae5afddbf130231aa82005b188a9047d9df8c1
xudojnik <AKryukov92@users.noreply.github.com>
2015-08-03 21:00:27 +0600

    ????????? ?????????? ?????????. ?????????? ????? ???????. ?????????? ?????? ????????.

---
+++ js/items/525.js
@@ -0,0 +1,38 @@
+{
+	"name":"Сигнальная ракетница",
+	"category":"pistol",
+	"mobility":100,
+	"clip":1,
+	"ammo":3,
+	"reload_cost":45,
+	"id":525,
+	"attacks":[
+	{
+		"name":"Удар",
+		"type":1,
+		"accuracy":90,
+		"cost":40,
+		"min_dist":0,
+		"max_dist":1.5,
+		"min_damage":10,
+		"max_damage":25
+	},
+	{
+		"name":"Навскидку",
+		"type":2,
+		"accuracy":52,
+		"cost":32,
+		"min_dist":8,
+		"max_dist":30,
+		"bullets":1,
+		"min_damage":20,
+		"max_damage":62,
+		"specials":[{
+			"duration":3,
+			"effects":[
+				"Наносит 14 ед. повреждения каждый раунд.",
+				"Уменьшает эффективность лечения на 30%"
+			]
+		}]
+	}]
+}
