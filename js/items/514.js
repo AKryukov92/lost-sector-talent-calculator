67897d7d114697bfabfdd59fe48efacfc7c55b43
xudojnik <xudojnik@localhost>
2015-09-27 01:46:15 +0600

    ????????? ?????? ??????

---
+++ js/items/514.js
@@ -0,0 +1,21 @@
+{
+	"id":514,
+	"name":"Связка гранат",
+	"category":"consumable",
+	"consumable_type":4,
+	"lvlreq":6,
+	"talentreq":25,
+	"classreq":["as","ju"],
+	"attacks":[{
+		"name":"Бросок",
+		"type":6,
+		"accuracy":45,
+		"radius":5,
+		"cost":65,
+		"min_dist":0,
+		"max_dist":12,
+		"min_damage":85,
+		"max_damage":140
+	}],
+	"ingredients":0
+}
