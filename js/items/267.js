12153b10fe8a65dd0bbcfd3e9313f592ce2c8409
xudojnikomsk <xudojnikomsk@gmail.com>
2016-12-11 00:18:31 +0600

    Add Interceptor helmet

---
+++ js/items/267.js
@@ -0,0 +1,14 @@
+{
+	"name":{
+		"ru":"Шлем 'Перехватчик'",
+		"en":"Interceptor Helmet"
+	},
+	"category":"hat",
+	"description":{
+		"ru":"Покупается в составе комплекта 'Прорыв'",
+		"en":"Part of 'Interceptor' set"
+	},
+	"protection":5,
+	"lvlreq":5,
+	"id":267
+}
